9027540d24e53cb55da9718b3d7ebed6d14f4ce0
Emilio Jeldes <emilio.jeldes@gmail.com>
2018-04-18 06:08:09 -0300

    starting the spring app

--- src/main/java/com/ewebs/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ewebs.repository;
-
-import com.ewebs.domain.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
